e2b180e1eed0ff5f51b626844cee617378ae3b2d
Khanh Nguyen <knguyen.ct@gmail.com>
2024-07-05 14:18:15 +0700

    add elasticsearch

--- apps/huge_seller/mix.exs
+++ apps/huge_seller/mix.exs
@@ -29,7 +29,8 @@ defmodule HugeSeller.MixProject do
       {:ecto_sql, "~> 3.0"},
       {:jason, "~> 1.4"},
       {:postgrex, ">= 0.0.0"},
-      {:tarams, "~> 1.0.0"}
+      {:tarams, "~> 1.0.0"},
+      {:elasticsearch, "~> 1.0.0"}
     ]
   end
 end
