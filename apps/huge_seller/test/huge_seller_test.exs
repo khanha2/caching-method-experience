c10f462233d0e68e1a0f30dc8210739bc47625ae
Khanh Nguyen <knguyen.ct@gmail.com>
2024-07-05 11:38:39 +0700

    remove unused code

--- apps/huge_seller/test/huge_seller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HugeSellerTest do
-  use ExUnit.Case
-  doctest HugeSeller
-
-  test "greets the world" do
-    assert HugeSeller.hello() == :world
-  end
-end
--- apps/huge_seller_api/test/huge_seller_api_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HugeSellerApiTest do
-  use ExUnit.Case
-  doctest HugeSellerApi
-
-  test "greets the world" do
-    assert HugeSellerApi.hello() == :world
-  end
-end
