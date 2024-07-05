92b5453bf162926977bd8290cfd4dea9740ec6e7
Khanh Nguyen <knguyen.ct@gmail.com>
2024-07-05 11:38:31 +0700

    add worker app

---
+++ apps/huge_seller_worker/lib/huge_seller_worker.ex
@@ -0,0 +1,18 @@
+defmodule HugeSellerWorker do
+  @moduledoc """
+  Documentation for `HugeSellerWorker`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HugeSellerWorker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
