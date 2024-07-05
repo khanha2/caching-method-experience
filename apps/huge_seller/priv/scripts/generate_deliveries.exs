5fe8725d3416b894e3e69ba15b586e474bb143c9
Khanh Nguyen <knguyen.ct@gmail.com>
2024-07-05 16:10:26 +0700

    add generating deliveries

---
+++ apps/huge_seller/priv/scripts/generate_deliveries.exs
@@ -0,0 +1,17 @@
+require Logger
+
+alias HugeSeller.Repo
+alias HugeSeller.Schema.Delivery
+
+Enum.each(1..50, fn delivery_id ->
+  %Delivery{}
+  |> Delivery.changeset(%{code: "delivery_#{delivery_id}", name: "Delivery #{delivery_id}"})
+  |> Repo.insert()
+  |> case do
+    {:ok, delivery} ->
+      Logger.info("Delivery #{delivery.code} created")
+
+    _error ->
+      nil
+  end
+end)
