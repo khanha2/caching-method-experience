da64a642a42c2a6ffdf1e52ecfa4d9f09c5e5497
Khanh Nguyen <knguyen.ct@gmail.com>
2024-07-09 15:41:05 +0700

    update shipment schema

--- apps/huge_seller/priv/repo/migrations/20240708033811_create_shipments.exs
+++ apps/huge_seller/priv/repo/migrations/20240708033811_create_shipments.exs
@@ -6,10 +6,16 @@ defmodule HugeSeller.Repo.Migrations.CreateShipments do
       add(:code, :text)
       add(:order_code, :text)
       add(:store_code, :text)
+      add(:warehouse_platform_code, :text)
+      add(:warehouse_shipment_code, :text)
       add(:warehouse_code, :text)
+      add(:delivery_platform_code, :text)
+      add(:tracking_code, :text)
       add(:package_code, :text)
       add(:type, :text)
       add(:status, :text)
+      add(:warehouse_status, :text)
+      add(:delivery_status, :text)
       add(:created_at, :naive_datetime)
 
       add(:order_id, references(:orders, on_delete: :nilify_all))
@@ -18,11 +24,13 @@ defmodule HugeSeller.Repo.Migrations.CreateShipments do
     end
 
     create(unique_index(:shipments, [:code]))
-    create(index(:shipments, [:order_code]))
-    create(index(:shipments, [:store_code]))
-    create(index(:shipments, [:warehouse_code]))
     create(index(:shipments, [:type]))
+    create(index(:shipments, [:warehouse_platform_code]))
+    create(index(:shipments, [:warehouse_code]))
     create(index(:shipments, [:status]))
+    create(index(:shipments, [:warehouse_status]))
+    create(index(:shipments, [:delivery_platform_code]))
+    create(index(:shipments, [:delivery_status]))
     create(index(:shipments, [:created_at]))
     create(index(:shipments, [:updated_at]))
   end
