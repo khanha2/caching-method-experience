5800c9912fbc0254bf2d88ac6d91735a6a090692
Khanh Nguyen <knguyen.ct@gmail.com>
2024-07-09 15:42:57 +0700

    update creating order cache

--- apps/huge_seller/lib/usecase/create_order_cache.ex
+++ apps/huge_seller/lib/usecase/create_order_cache.ex
@@ -4,6 +4,11 @@ defmodule HugeSeller.Usecase.CreateOrderCache do
   def perform(order) do
     order_code = order.code
 
+    platform_skus =
+      order.items
+      |> Enum.map(& &1.product_sku)
+      |> Enum.uniq()
+
     params = %{
       id: order_code,
       code: order.code,
@@ -11,6 +16,7 @@ defmodule HugeSeller.Usecase.CreateOrderCache do
       platform_status: order.platform_status,
       created_at: order.created_at,
       store_code: order.store_code,
+      platform_skus: platform_skus,
       shipments: Enum.map(order.shipments, &build_shipment_index(&1))
     }
 
@@ -18,6 +24,11 @@ defmodule HugeSeller.Usecase.CreateOrderCache do
   end
 
   defp build_shipment_index(shipment) do
+    warehouse_skus =
+      shipment.items
+      |> Enum.map(& &1.product_sku)
+      |> Enum.uniq()
+
     %{
       code: shipment.code,
       type: shipment.type,
@@ -27,7 +38,8 @@ defmodule HugeSeller.Usecase.CreateOrderCache do
       status: shipment.status,
       warehouse_status: shipment.warehouse_status,
       delivery_platform_code: shipment.delivery_platform_code,
-      delivery_status: shipment.delivery_status
+      delivery_status: shipment.delivery_status,
+      warehouse_skus: warehouse_skus
     }
   end
 end
