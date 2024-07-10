0277b93b224cf3ca6263f13ad124b0205197b572
Khanh Nguyen <knguyen.ct@gmail.com>
2024-07-10 16:05:11 +0700

    update test counting

--- apps/huge_seller/test/test_counting.exs
+++ apps/huge_seller/test/test_counting.exs
@@ -30,12 +30,34 @@ IO.inspect("ES: retrive #{es_count} orders with execution time is #{es_time / 1_
 
 IO.inspect("PG: retrive #{pg_count} orders with execution time is #{pg_time / 1_000} ms")
 
-IO.inspect("Retriving orders with warehoure status wh_packed")
+IO.inspect("Retriving orders with shipment warehoure status wh_new")
 
 params = %{
   page: 1000,
   size: 100,
-  shipment_warehouse_status: "xxx"
+  shipment_warehouse_status: "wh_new"
+}
+
+{:ok, es_query} = BuildOrderEsQuery.perform(params)
+
+{es_time, {:ok, es_count}} =
+  :timer.tc(fn -> Repository.count_es_orders(es_query, orders_index) end)
+
+IO.inspect("ES: retrive #{es_count} orders with execution time is #{es_time / 1_000} ms")
+
+{:ok, pg_query} = BuildOrderQuery.perform(params)
+
+{pg_time, pg_count} =
+  :timer.tc(fn -> Repository.count_orders(pg_query) end)
+
+IO.inspect("PG: retrive #{pg_count} orders with execution time is #{pg_time / 1_000} ms")
+
+IO.inspect("Retriving orders with shipment warehoure status wh_packed")
+
+params = %{
+  page: 1000,
+  size: 100,
+  shipment_warehouse_status: "wh_packed"
 }
 
 {:ok, es_query} = BuildOrderEsQuery.perform(params)
